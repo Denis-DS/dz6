aa839fbf64f798582da1d3a6fa11ba525775aea3
Denis-DS <56930333+Denis-DS@users.noreply.github.com>
2020-03-05 20:04:38 +0200

    Updates

---
+++ precache-manifest.1e0ea7c84db5c19a8b5e0bfd1c400549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f8a24f34df2b3845aa65093ea2f786",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f03a54fa084d6a9b1e05",
+    "url": "./static/css/main.8706273a.chunk.css"
+  },
+  {
+    "revision": "c839e581472cc05d2898",
+    "url": "./static/js/2.f9f28334.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "./static/js/2.f9f28334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03a54fa084d6a9b1e05",
+    "url": "./static/js/main.8d8079d9.chunk.js"
+  },
+  {
+    "revision": "fa0551c301ec8f18625f",
+    "url": "./static/js/runtime-main.61dbc9dd.js"
+  }
+]);
